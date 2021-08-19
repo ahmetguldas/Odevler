e3e8961e61e3bea2fefb8c4f4187b0ab764fa8e0
ahmet güldaş <ahmet.guldas@hotmail.com>
2021-08-19 21:07:11 +0300

    Add project files.

---
+++ Odevler/Controllers/UygulamaController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Odevler.Controllers
+{
+    public class UygulamaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> dizi = new() { "Ankara","Izmir", "Ïstanbul","Bursa" };
+            return View();
+        }
+
+        //gorev/yahoo
+        [Route("gorev/{id}")]
+        //uygulama/ara/yahoo
+        public IActionResult Ara(string id)
+        {
+            string site = "https://";
+            switch (id)
+            {
+                case "yandex": site += "www.yandex.com"; break;
+                case "google": site += "www.google.com"; break;
+                case "yahoo": site += "www.yahoo.com"; break;
+                default:
+                    site += "www.google.com";
+                    break;
+            }
+            return Redirect(site);
+        }
+
+       
+        
+
+
+
+    }
+}
---
+++ Odevler/TagHelpers/ListeleTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odevler.TagHelpers
+{
+    [HtmlTargetElement("dizi")]
+    public class ListeleTagHelper : TagHelper
+    {
+        public List<string> ogeler { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "listele";
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("<ul class='list-group'>");
+            string format = "<li class='list-group-item'></li>";
+            foreach (var item in ogeler)
+            {
+
+                stringBuilder.AppendFormat(format,item);
+            }
+            stringBuilder.Append("</ul>");
+            output.Content.SetHtmlContent(stringBuilder.ToString());
+            base.Process(context,output);
+        }
+    }
+}
